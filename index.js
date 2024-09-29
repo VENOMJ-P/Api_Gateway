8eaf53e53453438121760b3c9da56959466ff815
VENOMJ-P <jay63837@gmail.com>
2024-09-29 13:15:14 +0530

    Added all api

--- index.js
+++ index.js
@@ -1,46 +1,75 @@
 const express = require("express");
 const morgan = require("morgan");
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const rateLimit = require('express-rate-limit')
-const axios = require('axios');
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const rateLimit = require("express-rate-limit");
+const axios = require("axios");
+const dotenv = require("dotenv").config();
 
 const app = express();
 
-const PORT = 3005;
+const PORT = process.env.PORT;
 
 const limiter = rateLimit({
-    windowMs: 2 * 60 * 1000,
-    max: 5,
-})
+  windowMs: 2 * 60 * 1000,
+  max: 5,
+});
 
 app.use(morgan("combined"));
 app.use(limiter);
-app.use('/bookingservice', async (req, res, next) => {
-    try {
-        //console.log(req.headers['x-access-token']);
-        const response = await axios.get('http://localhost:3001/api/v1/isAuthenticated', {
-            headers: {
-                'x-access-token': req.headers['x-access-token'],
-            }
-        });
-        if(response.data.success){
-            next();
-        }else {
-            return res.status(401).json({
-                message:'Unauthorised'
-            })
-        }
-    } catch (error) {
-        return res.status(401).json({
-            message:'Unauthorised'
-        })
+app.use("/bookingservice", async (req, res, next) => {
+  try {
+    //console.log(req.headers['x-access-token']);
+    const response = await axios.get(
+      "http://localhost:3001/api/v1/isAuthenticated",
+      {
+        headers: {
+          "x-access-token": req.headers["x-access-token"],
+        },
+      }
+    );
+    if (response.data.success) {
+      next();
+    } else {
+      return res.status(401).json({
+        message: "Unauthorised",
+      });
     }
-})
-app.use('/bookingservice', createProxyMiddleware({ target: 'http://localhost:3002/', changeOrigin: true }));
-app.get('/home', (req, res) => {
-    return res.json({ message: 'OK' })
-})
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unauthorised",
+    });
+  }
+});
+
+app.use(
+  "/bookingService",
+  createProxyMiddleware({
+    target: "http://localhost:3002/",
+    changeOrigin: true,
+  })
+);
+app.use(
+  "/authService",
+  createProxyMiddleware({
+    target: "http://localhost:3001/",
+    changeOrigin: true,
+  })
+);
+app.use(
+  "/searchService",
+  createProxyMiddleware({
+    target: "http://localhost:3000/",
+    changeOrigin: true,
+  })
+);
+app.use(
+  "/reminderService",
+  createProxyMiddleware({
+    target: "http://localhost:3003/",
+    changeOrigin: true,
+  })
+);
 
 app.listen(PORT, () => {
-    console.log(`Server started at ${PORT}`);
-}) 
+  console.log(`Server started at ${PORT}`);
+});
